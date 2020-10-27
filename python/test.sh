8c469278954ff42ed9f266bcb70a93e101a7634f
W. Nicholas Greene <wng@csail.mit.edu>
2020-10-26 21:38:44 -0400

    Add test script.

---
+++ python/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python python/test.py --image_path ~/Projects/data/gta_sfm_colmap/test/20190127_154841/images --sparse_path ~/Projects/data/gta_sfm_colmap/test/20190127_154841/sparse --output_path output --image_width 640 --image_height 480

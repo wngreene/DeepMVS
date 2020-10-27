53b46104c8fcbdf9f6d547ba290c2e1b6904d7c5
W. Nicholas Greene <wng@csail.mit.edu>
2020-10-26 22:13:00 -0400

    Clean up test script.

--- python/test.sh
+++ python/test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-python python/test.py --image_path ~/Projects/data/gta_sfm_colmap/test/20190127_154841/images --sparse_path ~/Projects/data/gta_sfm_colmap/test/20190127_154841/sparse --output_path output --image_width 640 --image_height 480
+python python/test.py --image_path ~/Projects/data/gta_sfm_colmap/test/20190127_154841/images \
+       --sparse_path ~/Projects/data/gta_sfm_colmap/test/20190127_154841/sparse \
+       --output_path output \
+       --image_width 640 --image_height 480 \
+       --max_num_neighbors 1
